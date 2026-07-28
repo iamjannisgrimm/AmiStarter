e285989c4b5ab8125dc4d92cd39554b0b9a4171e
Jannis Grimm <jannis@grimm.me>
2026-07-28 11:33:06 +0200

    Initial Commit

---
+++ AmiStarter/AmiStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmiStarterApp.swift
+//  AmiStarter
+//
+//  Created by Jannis on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct AmiStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
